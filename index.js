ecdc16d20170ae705f7057c2c87dd6f46f6b5619
Denis <test@test.ru>
2018-02-27 23:03:18 +0300

    copy-resource-files

---
+++ index.js
@@ -0,0 +1,124 @@
+const loaderUtils = require('loader-utils'),
+    glob = require('glob'),
+    path = require('path'),
+    minifier = require('html-minifier').minify;
+
+function minify(content) {
+    content = content instanceof Buffer ? content.toString('utf8') : content;
+    try {
+        return minifier(content, {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            minifyCSS: true,
+            minifyJS: true,
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+        });
+    } catch (e) {
+        console.warn(path);
+        return content;
+    }
+};
+
+function parseGlob(glob) {
+    let str = glob.replace(/\*\*/g, '').replace(/[\+\-@]/g, '').replace(/\*/g, '[\\w\\-\\.\\/]+').replace(/\./g, '\.').replace(/\//g, '/?');
+    return str;
+};
+
+function CopyResorceFiles(options) {
+    this.options = options || {};
+};
+
+CopyResorceFiles.prototype.apply = function(compiler) {
+    let src = this.options.src, isMin = this.options.min, name = this.options.name || null;
+    if (src) {
+        compiler.plugin('emit', (compilation, callback) => {
+            glob(src, {root: this.options.root || compiler.context, ignore: this.options.ignore || null}, (err, files) => {
+                let srcReg = new RegExp(parseGlob(src), 'ig');
+                let findFiles = files.reduce((prev, file) => {
+                    let basename = path.basename(file);
+                    if (!prev[basename]) prev[basename] = file;
+                    else if(Array.isArray(prev[basename])) {
+                        prev[basename].push(file);
+                    } else {
+                        prev[basename] = [prev[basename]];
+                    }
+                    return prev;
+                }, {});
+
+                let context = compilation.entries.map(entry => entry.context);
+                if (context.length) {
+                    compilation.chunks.forEach(chunk => {
+                        chunk.files.forEach(file => {
+                            let source = compilation.assets[file].source().replace(srcReg, (...find) =>  {
+                                let str = find[0], filePath;
+                                context.some(entry => {
+                                    try {
+                                        let need;
+                                        let baseName = path.basename(str);
+                                        if (baseName in findFiles) {
+                                            if (Array.isArray(findFiles[baseName])) {
+                                                need = findFiles[baseName].find(it => it.endsWith(str));
+                                            } else need = findFiles[baseName].endsWith(str) ? findFiles[baseName] : false;
+                                        }
+                                        if (need) {
+                                            let resolve = path.resolve(entry, '../' + need)
+                                            let stat = compiler.inputFileSystem.statSync(resolve);
+                                            if (stat.isFile()) {
+                                                filePath = resolve;
+                                                return true
+                                            }
+                                        } else return false;
+                                    } catch (e) {
+                                        console.warn(e);
+                                        return false;
+                                    }
+                                });
+                                if (filePath) {
+                                    let fileContext = compiler.inputFileSystem.readFileSync(filePath);
+                                    let newFileName;
+                                    if (name) {
+                                        if (name.search('[hash]')) {
+                                            newFileName = name.replace('[hash]', loaderUtils.getHashDigest(fileContext));
+                                        }
+                                    } else {
+                                        newFileName = path.basename(filePath);
+                                    }
+                                    if (isMin) fileContext = minify(fileContext);
+                                    compilation.assets[newFileName] = {
+                                        source: function() {
+                                            return fileContext;
+                                        },
+                                        size: function() {
+                                            return fileContext.length;
+                                        }
+                                    };
+                                    return newFileName;
+                                } else return str;
+                            });
+                            compilation.assets[file] = {
+                                source: function () {
+                                    return source;
+                                },
+                                size: function() {
+                                    return source.length;
+                                }
+                            };
+                        });
+                    });
+                }
+
+                callback();
+            });
+        });
+
+        compiler.plugin('done', () => {
+            console.log('All resources are copied');
+        });
+    }
+};
+
+module.exports = CopyResorceFiles;
