524d0ec347a560659d60e1e59acc3d6424a798ab
Denis <test@test.ru>
2018-03-05 14:46:50 +0300

    Рекурсивное извлечение ресурсов

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function CopyResorceFiles(options) {
 };
 
 CopyResorceFiles.prototype.apply = function(compiler) {
-    let src = this.options.src, isMin = this.options.min, name = this.options.name || null;
+    let src = this.options.src, isMin = this.options.min, name = this.options.name || null, recurse = this.options.recurse;
     if (src) {
         compiler.plugin('compilation', (compilation) => {
             compilation.plugin('additional-assets', (callback) => {
@@ -51,50 +51,66 @@ CopyResorceFiles.prototype.apply = function(compiler) {
                         return prev;
                     }, {});
 
+                    /**
+                     * Заменяет ресурсы
+                     * @param source
+                     * @returns {null | string} : null - если не было замены, иначе string
+                     */
+                    function replaceSource(source) {
+                        let hasReplace = false;
+                        source = source instanceof Buffer ? source.toString('utf8') : source;
+                        let newSource = source.replace(srcReg, (...find) =>  {
+                            let str = find[0], filePath;
+                            context.some(entry => {
+                                try {
+                                    let need;
+                                    let baseName = path.basename(str);
+                                    if (baseName in findFiles) {
+                                        if (Array.isArray(findFiles[baseName])) {
+                                            need = findFiles[baseName].find(it => it.endsWith(str));
+                                        } else need = findFiles[baseName].endsWith(str) ? findFiles[baseName] : false;
+                                    }
+                                    if (need) {
+                                        let resolve = path.resolve(entry, '../' + need)
+                                        let stat = compiler.inputFileSystem.statSync(resolve);
+                                        if (stat.isFile()) {
+                                            filePath = resolve;
+                                            return true
+                                        }
+                                    } else return false;
+                                } catch (e) {
+                                    console.warn(e);
+                                    return false;
+                                }
+                            });
+                            if (filePath) {
+                                let fileContext = compiler.inputFileSystem.readFileSync(filePath);
+                                let newFileName;
+                                if (name) {
+                                    if (name.search('[hash]')) {
+                                        newFileName = name.replace('[hash]', loaderUtils.getHashDigest(fileContext));
+                                    }
+                                } else {
+                                    newFileName = path.basename(filePath);
+                                }
+                                let replacedContext = recurse ? replaceSource(fileContext) : null;
+                                if (replacedContext) fileContext = replacedContext;
+                                if (isMin) fileContext = minify(fileContext);
+                                compilation.assets[newFileName] = new webpackSources.RawSource(fileContext);
+                                hasReplace = true;
+                                return newFileName;
+                            } else return str;
+                        });
+
+                        return hasReplace ? newSource : null;
+                    }
+
                     let context = compilation.entries.map(entry => entry.context);
                     if (context.length) {
                         compilation.chunks.forEach(chunk => {
                             chunk.files.forEach(file => {
-                                let source = compilation.assets[file].source().replace(srcReg, (...find) =>  {
-                                    let str = find[0], filePath;
-                                    context.some(entry => {
-                                        try {
-                                            let need;
-                                            let baseName = path.basename(str);
-                                            if (baseName in findFiles) {
-                                                if (Array.isArray(findFiles[baseName])) {
-                                                    need = findFiles[baseName].find(it => it.endsWith(str));
-                                                } else need = findFiles[baseName].endsWith(str) ? findFiles[baseName] : false;
-                                            }
-                                            if (need) {
-                                                let resolve = path.resolve(entry, '../' + need)
-                                                let stat = compiler.inputFileSystem.statSync(resolve);
-                                                if (stat.isFile()) {
-                                                    filePath = resolve;
-                                                    return true
-                                                }
-                                            } else return false;
-                                        } catch (e) {
-                                            console.warn(e);
-                                            return false;
-                                        }
-                                    });
-                                    if (filePath) {
-                                        let fileContext = compiler.inputFileSystem.readFileSync(filePath);
-                                        let newFileName;
-                                        if (name) {
-                                            if (name.search('[hash]')) {
-                                                newFileName = name.replace('[hash]', loaderUtils.getHashDigest(fileContext));
-                                            }
-                                        } else {
-                                            newFileName = path.basename(filePath);
-                                        }
-                                        if (isMin) fileContext = minify(fileContext);
-                                        compilation.assets[newFileName] = new webpackSources.RawSource(fileContext);
-                                        return newFileName;
-                                    } else return str;
-                                });
-                                compilation.assets[file] = new webpackSources.RawSource(source);
+                                let source = replaceSource(compilation.assets[file].source());
+                                if (source) compilation.assets[file] = new webpackSources.RawSource(source);
                             });
                         });
                     }
